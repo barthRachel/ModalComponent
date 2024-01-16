3ec40064cad1e3f0ed1f98cc9671f87085d0f897
Rachel Barthélery <rachel-barth@live.fr>
2024-01-16 20:17:01 +0100

    MAJ - Modifications pour deploiement sur npm

--- dist/Modal.js
+++ dist/Modal.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = Modal;
+var _react = _interopRequireDefault(require("react"));
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 require("./Modal.css");
 var _propTypes = _interopRequireDefault(require("prop-types"));
@@ -24,13 +25,13 @@ function Modal(_ref) {
   var Modal = _styledComponents["default"].div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n        position: relative;\n        z-index: 20;\n        display: flex;\n        flex-direction: column;\n        align-items: flex-end;\n        padding: 30px 45px;\n        border-radius: 20px;\n        background-color: white;\n        box-shadow: 0 0 10px #", ";\n    "])), shadowModalColor ? shadowModalColor : "000000");
   var ButtonCross = _styledComponents["default"].button(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n        background-color: #", ";\n        color: white;\n        padding: 7px 10px;\n        border-radius: 100%;\n        border: 2px solid transparent;\n        transition: all 0.25s ease-in-out;\n        position: absolute;\n        top: -14px;\n        right: -17px;\n\n        &:hover {\n            background-color: white;\n            color: #", ";\n            border: 2px solid #", ";\n            cursor: pointer;\n        }\n    "])), buttonColor ? buttonColor : "000000", buttonColor ? buttonColor : "000000", buttonColor ? buttonColor : "000000");
   var ButtonConfirmation = _styledComponents["default"].button(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n        margin-top: 25px;\n        background-color: #", ";\n        color: white;\n        padding: 5px 10px;\n        border-radius: 5px;\n        border: 2px solid transparent;\n        transition: all 0.25s ease-in-out;\n\n        &:hover {\n            background-color: white;\n            color: #", ";\n            border: 2px solid #", ";\n            cursor: pointer;\n        }\n    "])), buttonColor ? buttonColor : "000000", buttonColor ? buttonColor : "000000", buttonColor ? buttonColor : "000000");
-  return /*#__PURE__*/React.createElement(ModalWrapper, {
+  return /*#__PURE__*/_react["default"].createElement(ModalWrapper, {
     className: isOpen ? "modal-open" : "modal-close"
-  }, /*#__PURE__*/React.createElement(Modal, null, cross && /*#__PURE__*/React.createElement(ButtonCross, {
+  }, /*#__PURE__*/_react["default"].createElement(Modal, null, cross && /*#__PURE__*/_react["default"].createElement(ButtonCross, {
     onClick: onClose
-  }, "X"), /*#__PURE__*/React.createElement("h2", {
+  }, "X"), /*#__PURE__*/_react["default"].createElement("h2", {
     className: "modal-text"
-  }, text), (button || cross === undefined) && /*#__PURE__*/React.createElement(ButtonConfirmation, {
+  }, text), (button || cross === undefined) && /*#__PURE__*/_react["default"].createElement(ButtonConfirmation, {
     onClick: onClose
   }, buttonText === undefined ? "Ok" : buttonText)));
 }
--- src/components/ModalComponent/Modal.jsx
+++ src/components/ModalComponent/Modal.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import './Modal.css';
 import propTypes from 'prop-types';
